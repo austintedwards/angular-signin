649e64839a9e6182784d256a6f65aff4e5d4ebea
Austin Edwards <austintedwards@gmail.com>
2017-02-10 13:28:21 -0700

    gifs with delete

--- scripts/app.js
+++ scripts/app.js
@@ -14,19 +14,24 @@
         const dat = this
         // dat.newPost = newPost;
         dat.$onInit = grabGifs;
+        dat.clickDelete = clickDelete
 
 
 
         function grabGifs() {
           var gifs =[];
-          $http.get('http://api.giphy.com/v1/gifs/search?q=fart&api_key=dc6zaTOxFJmzC')
+          $http.get('http://api.giphy.com/v1/gifs/search?q=dance&limit=100&api_key=dc6zaTOxFJmzC')
            .then(function (res) {
-             for (var i = 0; i < 25; i++) {
+             for (var i = 0; i < 100; i++) {
                gifs.push(res.data.data[i].images.fixed_height.url)
              }
-             console.log(gifs)
              dat.gifs=gifs
              });
+           }
+
+           function clickDelete(gif){
+             dat.gifs.splice(dat.gifs.indexOf(gif), 1);
+
            }
 
 
